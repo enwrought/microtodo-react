26ce8154df8cf56e52cc99c54c0a529d69b27ddf
Bryant Lin <blin@guidewire.com>
2017-04-16 23:51:29 -0700

    Added redux

--- modules/Home.js
+++ modules/Home.js
@@ -1,58 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import { colorPalette } from './colorPalette';
 import { taskSort } from './taskSort';
 import { toStringTime } from './utils';
 import Task from './Task';
 
-@colorPalette
+@connect(state => {
+  return { tasks: state.tasks };
+})
 export default class Home extends Component {
-  task_array = [
-    {
-      taskId: 0,
-      name: 'Explore Store design',
-      estimate: 90,
-      description: 'Take a look at localStorage',
-      completed: false,
-      dueDate: '09/20/16',
-      color: Home.getNextColor()
-    },
-    {
-      taskId: 1,
-      name: 'Use store instead of default',
-      estimate: 60,
-      completed: false,
-      dueDate: '09/23/16',
-      color: Home.getNextColor()
-    },
-    {
-      taskId: 2,
-      name: 'Read Zone to Win',
-      estimate: 240,
-      completed: false,
-      dueDate: '09/19/16',
-      color: Home.getNextColor()
-    },
-    {
-      taskId: 3,
-      name: 'Write stories/design algorithm for scheduling',
-      estimate: 45,
-      completed: false,
-      dueDate: '09/25/16',
-      color: Home.getNextColor()
-    },
-    {
-      taskId: 4,
-      name: 'Make ColorPalette a decorator',
-      estimate: 45,
-      completed: false,
-      dueDate: '09/30/16',
-      color: Home.getNextColor()
-    }
-  ];
+  static propTypes = {
+    tasks: PropTypes.arrayOf(PropTypes.object).isRequired
+  };
 
   // TODO: find some way to not recompute this every render
   generateTasks() {
-    return taskSort(this.task_array).map(props =>
+    return taskSort(this.props.tasks).map(props =>
       (<Task key={ props.taskId } { ...props } />));
   }
 
@@ -63,7 +26,7 @@ export default class Home extends Component {
     const remainingTime = `Remaining time: ${toStringTime(157)}`;
 
     // TODO: check to see if the task is complete before including it
-    const tasksLeft = `Tasks left: ${this.task_array.length}`;
+    const tasksLeft = `Tasks left: ${this.props.tasks.length}`;
 
     return (
       <div className="home">
---
+++ modules/tmpData.js
@@ -0,0 +1,45 @@
+import { COLORS } from './colorPalette';
+
+export default [
+  {
+    taskId: 0,
+    name: 'Explore Store design',
+    estimate: 90,
+    description: 'Take a look at localStorage',
+    completed: false,
+    dueDate: '09/20/16',
+    color: COLORS.sunflower
+  },
+  {
+    taskId: 1,
+    name: 'Use store instead of default',
+    estimate: 60,
+    completed: false,
+    dueDate: '09/23/16',
+    color: COLORS.sunflower
+  },
+  {
+    taskId: 2,
+    name: 'Read Zone to Win',
+    estimate: 240,
+    completed: false,
+    dueDate: '09/19/16',
+    color: COLORS.sunflower
+  },
+  {
+    taskId: 3,
+    name: 'Write stories/design algorithm for scheduling',
+    estimate: 45,
+    completed: false,
+    dueDate: '09/25/16',
+    color: COLORS.sunflower
+  },
+  {
+    taskId: 4,
+    name: 'Make ColorPalette a decorator',
+    estimate: 45,
+    completed: false,
+    dueDate: '09/30/16',
+    color: COLORS.sunflower
+  }
+];
